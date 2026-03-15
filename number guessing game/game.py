1d50f8fbb443e5305723287b52fe1f623ae42842
tanuja47 <btanuja303@gmail.com>
2026-03-15 16:50:09 +0530

    added number guessing game project

---
+++ number guessing game/game.py
@@ -0,0 +1,22 @@
+
+import random 
+def guessing_number():
+    print("welcome to number guessing game!!")
+    print("guess number from 1 to 100")
+    number=random.randint(1,100)
+    attempts=0
+    while True:
+        guess=int(input("enter the number:"))
+        if guess<number:
+            print("Too low!!")
+            attempts+=1
+        elif guess>number:
+            print("Too high!!")
+            attempts+=1
+        elif guess==number:
+            print("yeahh!! you guessed correct!!")
+            print(f"tou guessed in {attempts} attempts")
+            break
+        else:
+            print("not valid")
+guessing_number()
